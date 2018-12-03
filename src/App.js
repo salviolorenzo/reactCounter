2d8ced2d37e7c5ededbdc1eeba0f762800493f4a
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-03 10:44:14 -0500

    adds rendering of scores

--- src/App.js
+++ src/App.js
@@ -4,6 +4,48 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    // Set up state
+    this.state = {
+      scores: [
+        {
+          id: 1,
+          name: "alice",
+          score: 1001
+        },
+        {
+          id: 2,
+          name: "jeff",
+          score: 20
+        },
+        {
+          id: 3,
+          name: "stan",
+          score: 500
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Scoreboard App</h1>
+        <div className="score-container">{this._scoresAsCards()}</div>
+      </div>
+    );
+  }
+
+  _scoresAsCards() {
+    const cards = this.state.scores.map(score => {
+      return (
+        <div key={score.id}>
+          <h2>
+            Name: {score.name}, Score: {score.score}
+          </h2>
+        </div>
+      );
+    });
+    return cards;
   }
 }
 
