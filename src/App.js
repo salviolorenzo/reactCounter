76790088dec9260b0af0a4408df5363a548eabfb
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-03 11:40:34 -0500

    adds spread operator

--- src/App.js
+++ src/App.js
@@ -42,10 +42,58 @@ class App extends Component {
           <h2>
             Name: {score.name}, Score: {score.score}
           </h2>
+          <button
+            onClick={() => {
+              this._incrementScoreById(score.id);
+            }}
+          >
+            +
+          </button>
         </div>
       );
     });
     return cards;
+  }
+
+  // Version 1: using .map and manually updating values
+  // _incrementScoreById(id) {
+  //   // find the player in this.state.scores
+  //   // increment the score
+  //   const newScores = this.state.scores.map(score => {
+  //     if (score.id !== id) {
+  //       return score;
+  //     } else {
+  //       return {
+  //         id: score.id,
+  //         name: score.name,
+  //         score: score.score + 1
+  //       };
+  //     }
+  //   });
+  //   // call this.setState
+  //   this.setState({
+  //     scores: newScores
+  //   });
+  // }
+
+  // Version 2: using .map and shorthand to copy values from the original
+  _incrementScoreById(id) {
+    // find player
+    // increment score
+    const newScores = this.state.scores.map(score => {
+      if (score.id !== id) {
+        return score;
+      } else {
+        return {
+          ...score,
+          score: score.score + 1
+        };
+      }
+    });
+    // call setState
+    this.setState({
+      scores: newScores
+    });
   }
 }
 
